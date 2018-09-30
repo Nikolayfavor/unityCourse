4195a75f4b55b430774fe43f7d71d7eb43299e2c
ghMaxi <ec.qwertyui@gmail.com>
2018-09-30 15:38:59 +0300

    Скрипт PlayerInput и сцена с бегающим по стрелочками и WASD персонажем

---
+++ ShP2018/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+	public float speed = 5;
+	Animator a;
+	void Awake() {
+		a = GetComponent<Animator>();
+	}
+	void Update() {
+		Vector2 motion = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		a.SetFloat("motionX", motion.x);
+		a.SetFloat("motionZ", motion.y);
+		motion *= Time.deltaTime * speed;
+		transform.Translate(motion.x, 0, motion.y, Space.Self);
+	}
+}
